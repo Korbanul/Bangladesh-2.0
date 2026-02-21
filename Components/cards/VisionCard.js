42f590d61d35cb3e5d4ed09c7cb431b3cef3f356
Korbanul <gazikorbanulislam@gmail.com>
2026-02-21 12:14:29 +0600

    Fixed Card Alignment

--- Components/cards/VisionCard.js
+++ Components/cards/VisionCard.js
@@ -6,14 +6,14 @@ import "@/style/Cardsection.css"
 
 export default function visionCard({ visiondtails }) {
     let buttontype = visiondtails.buttontype
-    let bgcolor=visiondtails.cardcolor
+    let bgcolor = visiondtails.cardcolor
     return (
-        <Card className='p-3 cardstyle h-100' style={{backgroundColor:bgcolor}}>
+        <Card className='p-3 cardstyle h-100' style={{ backgroundColor: bgcolor }}>
             <Card.Img variant="top" src="./enegrylight.png" className="PillarsImg" />  {/* if mx-auto applied image will be in center*/}
-            <Card.Body>
-                <Card.Title>{visiondtails.title}</Card.Title>
-                <Card.Text>{visiondtails.description} </Card.Text>
-            </Card.Body>
+
+            <Card.Title className='mt-3 '>{visiondtails.title}</Card.Title>
+            <Card.Text className='text-muted'>{visiondtails.description} </Card.Text>
+
             <CustomButton variant={buttontype}>
                 Learn More
             </CustomButton>
--- Components/cards/pillarsCard.js
+++ Components/cards/pillarsCard.js
@@ -3,14 +3,20 @@ import Card from 'react-bootstrap/Card';
 import "@/style/Cardsection.css"
 
 function PillarsCard({ pillarsDtails }) {
-    let bgcolor=pillarsDtails.cardcolor
+    let bgcolor = pillarsDtails.cardcolor
     return (
-        <Card className='p-3 cardstyle' style={{backgroundColor:bgcolor}}>
-            <Card.Img variant="top" src="./enegrylight.png" className="PillarsImg"/>  {/* if mx-auto applied image will be in center*/}
-            <Card.Body>
-                <Card.Title>{pillarsDtails.title}</Card.Title>
-                <Card.Text>{pillarsDtails.description} </Card.Text>
-            </Card.Body>
+        <Card className='p-3 cardstyle' style={{ backgroundColor: bgcolor }}>
+            <Card.Img variant="top" src="./enegrylight.png" className="PillarsImg" />  {/* if mx-auto applied image will be in center*/}
+            
+            {/* If i use card.Body then the alignment of the image and title, description mismatch */}
+
+            <Card.Title className='mt-3'>
+                {pillarsDtails.title}
+            </Card.Title>
+
+            <Card.Text className="text-muted">
+                {pillarsDtails.description}
+            </Card.Text>
         </Card>
     );
 }
