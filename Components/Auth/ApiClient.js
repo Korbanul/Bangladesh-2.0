73b397058666e0880da8962915b52cc94ada9af7
Korbanul <gazikorbanulislam@gmail.com>
2026-03-14 00:02:22 +0600

    Backend deployed route added

--- Components/Auth/ApiClient.js
+++ Components/Auth/ApiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://bangladesh-20-backend-production.up.railway.app/";
 export async function apiClient(endpoint,options={}) {
     const response = await fetch(`${API_BASE_URL}${endpoint}`,
         {
